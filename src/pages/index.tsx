835c16a0f138738736bab6dd5ed5989825fd8cb5
Lucas <lucasazzollinivieira@gmail.com>
2022-05-15 22:44:36 -0300

    chore: Pages index of imports

---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+export { default as IntroPage } from "./IntroPage";
+export { default as AboutMePage } from "./AboutMePage";
+export { default as ProjectPage } from "./ProjectPage";
+export { default as ContactPage } from "./ContactPage";
