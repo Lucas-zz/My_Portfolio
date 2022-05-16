28ed0d526f03cfa2aa7639fc27ee3c5c621f1596
Lucas <lucasazzollinivieira@gmail.com>
2022-05-15 22:43:33 -0300

    feat: initial configs on IntroPage  done

---
+++ src/pages/IntroPage/index.tsx
@@ -0,0 +1,11 @@
+import Footer from "../../components/Footer";
+import { Container } from "./style";
+
+export default function IntroPage() {
+    return (
+        <>
+            <Container />
+            <Footer />
+        </>
+    );
+}
