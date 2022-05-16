71c9c213d6f6d581dcb90f15678d07fc80737ec8
Lucas <lucasazzollinivieira@gmail.com>
2022-05-15 22:43:58 -0300

    feat: initial configs on ProjectPage done

---
+++ src/pages/ProjectPage/index.tsx
@@ -0,0 +1,11 @@
+import Footer from "../../components/Footer";
+import { Container } from "./style";
+
+export default function ProjectPage() {
+    return (
+        <>
+            <Container />
+            <Footer />
+        </>
+    );
+}
---
+++ src/pages/ProjectPage/style.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.main`
+    width: 100%;
+    height: 100vh;
+
+    background-color: #3F61D7;
+`;
+
+export { Container }
