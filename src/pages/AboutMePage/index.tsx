b8a3223b06614012275522ae8c2054df7f9e7632
Lucas <lucasazzollinivieira@gmail.com>
2022-05-15 22:43:04 -0300

    feat: initial configs on AboutMePage done

---
+++ src/pages/AboutMePage/index.tsx
@@ -0,0 +1,11 @@
+import Footer from "../../components/Footer";
+import { Container } from "./style";
+
+export default function AboutMePage() {
+    return (
+        <>
+            <Container />
+            <Footer />
+        </>
+    );
+}
