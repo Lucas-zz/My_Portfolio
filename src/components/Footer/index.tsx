1ee2ebe6bfdcd3a6d669b684a657a9f3f8611c24
Lucas <lucasazzollinivieira@gmail.com>
2022-05-15 22:40:46 -0300

    feat: footer v1.0 done

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+import { ContactInfo, Container, CurrentAddress, Logo, Socials } from "./style";
+import { LinkedIn, Twitter, GitHub } from '@mui/icons-material';
+
+export default function Footer() {
+    return (
+        <Container>
+            <Logo>
+                <h3 title="Lucas Azzolini Vieira">L.A.V.</h3>
+                <p>Made with React</p>
+            </Logo>
+            <CurrentAddress title="Entre em contato comigo para mais informações">
+                Rua Maringá, 105, Blumenau
+                <br></br>
+                Santa Catarina, Brasil
+            </CurrentAddress>
+            <ContactInfo>
+                lucasazzollinivieira@gmail.com
+                <br></br>
+                +55 (47) 9 9698-8923
+            </ContactInfo>
+            <Socials>
+                <nav>
+                    <a href="https://www.linkedin.com/in/azzolinilucas/" title="LinkedIn">
+                        <LinkedIn sx={{ fontSize: 46, color: "#111" }} />
+                    </a>
+                    <a href="https://twitter.com/zulenno" title="Twitter">
+                        <Twitter sx={{ fontSize: 46, color: "#111" }} />
+                    </a>
+                    <a href="https://github.com/Lucas-zz" title="GitHub">
+                        <GitHub sx={{ fontSize: 46, color: "#111" }} />
+                    </a>
+                </nav>
+            </Socials >
+        </Container >
+    );
+}
---
+++ src/components/Footer/style.tsx
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+
+const Container = styled.footer`
+    width: 100%;
+    height: 200px;
+
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+    padding: 32px 40px;
+
+    margin: 0 auto;
+
+    h3 {
+        font-family: "Abel", sans-serif;
+    }
+
+    p {
+        font-family: "Source Code Pro", monospace;
+    }
+`;
+
+const Logo = styled.div`
+    width: 30%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+
+    h3 {        
+        font-size: 32px;
+        font-weight: 400;
+        line-height: 34px;
+        color: #111;
+
+        margin: 0 0 32px;
+    }
+
+    p {
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 24px;
+        color: #111;
+
+        margin: 16px 0 0;
+    }
+`;
+
+const CurrentAddress = styled.p`
+    width: 25%;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
+    color: #111;
+`;
+
+const ContactInfo = styled.p`
+    width: 25%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
+    color: #111;
+`;
+
+const Socials = styled.div`
+    width: 20%;
+
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-start;
+
+    nav {
+        width: 100%;
+
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+
+        & > a {
+            display: flex;
+            align-items: center;
+        }
+    }
+`;
+
+export {
+    Container,
+    ContactInfo,
+    CurrentAddress,
+    Logo,
+    Socials,
+}
