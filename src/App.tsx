63f161247c5d18afe96e975f9a91dfbcd92e7a1c
Lucas <lucasazzollinivieira@gmail.com>
2022-05-15 22:46:43 -0300

    chore: initial styles, provider and routes configs done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AlertProvider } from './contexts/alertContext';
+import { GlobalStyle } from "./style/reset";
+import { IntroPage, AboutMePage, ProjectPage, ContactPage } from './pages';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<AlertProvider>
+				<BrowserRouter>
+					<Header />
+					<Routes>
+						<Route path="/" element={<IntroPage />} />
+						<Route path="/about-me" element={<AboutMePage />} />
+						<Route path="/projects" element={<ProjectPage />} />
+						<Route path="/contact" element={<ContactPage />} />
+					</Routes>
+				</BrowserRouter>
+			</AlertProvider>
+		</>
+	);
 }
 
 export default App;
