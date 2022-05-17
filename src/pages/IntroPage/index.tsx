a08e0c9d800b366a564256d5edf48e5ed807b491
Lucas <lucasazzollinivieira@gmail.com>
2022-05-16 22:19:34 -0300

    feat: IntroPage partially done

--- src/pages/IntroPage/index.tsx
+++ src/pages/IntroPage/index.tsx
@@ -1,10 +1,58 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Container, Navigator, QuoteSection } from "./style";
 import Footer from "../../components/Footer";
-import { Container } from "./style";
 
 export default function IntroPage() {
+
+    let quoteNumber = 0;
+
+    window.scrollTo(0, 0);
+
+    randomQuotePicker();
+
+    const quotes = [
+        {
+            message: "Theory is when you know something, but it doesn't work. Practice is when something works, but you don't know why. Programmers combine theory and practice: Nothing works and they don't know why.",
+            author: "Unknown"
+        },
+        {
+            message: "Documentation is a love letter that you write to your future self.",
+            author: "Damian Conway"
+        },
+        {
+            message: "Anyone can write code that a computer can understand. Good programmers write code that humans can understand.",
+            author: "Martin Fowler"
+        },
+        {
+            message: "Sometimes it's better to leave something alone, to pause, and that's very true for programming.",
+            author: "Joyce Wheeler"
+        }
+    ];
+
+    function randomQuotePicker(): number {
+        quoteNumber = Math.floor(Math.random() * 4);
+
+        return quoteNumber;
+    }
+
     return (
         <>
-            <Container />
+            <Container>
+                <Navigator>
+                    <div>
+                        <Link to="/about-me">Sobre Mim</Link>
+                        <Link to="/projects">Projetos</Link>
+                        <Link to="/contact">Contato</Link>
+                    </div>
+                </Navigator>
+                <QuoteSection>
+                    <div>
+                        <p>{quotes[quoteNumber].message}</p>
+                        <span>- {quotes[quoteNumber].author}</span>
+                    </div>
+                </QuoteSection>
+            </Container>
             <Footer />
         </>
     );
--- src/pages/IntroPage/style.tsx
+++ src/pages/IntroPage/style.tsx
@@ -2,8 +2,107 @@ import styled from "styled-components";
 
 const Container = styled.main`
     width: 100%;
-    height: 100vh;
-    background-color: #3F61D7;
+    min-height: 100vh;
 `;
 
-export { Container }
+const Navigator = styled.section`
+    width: 100%;
+    height: 85vh;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-image: url(https://blog.imgur.com/wp-content/uploads/2016/05/naa3bIG.gif);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+
+    filter: brightness(0.8) contrast(1.1);
+
+    div {
+        width: 80%;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+
+        padding: 10vmax 4vw;
+        
+        margin: 0 auto;
+
+        a {
+            font-family: "Abel", sans-serif; 
+            font-size: calc((4 - 1) * 1.2vw + 1rem);
+            font-weight: 400;
+            line-height: 56px;
+
+            padding-left: 17px;
+            padding-right: 17px;
+
+            margin: 2vw 0;
+
+            text-decoration-line: underline;
+            text-decoration-style: solid;
+            text-decoration-thickness: 1.5px;
+            text-underline-offset: 1rem;
+
+            text-shadow: .6px .6px #111;
+
+            color: #FFF;
+
+            :hover {
+                filter: brightness(1.5);
+            }
+        }
+    }
+`;
+
+const QuoteSection = styled.section`
+    width: 100%;
+    height: 60vh;
+
+    background-color: #253551;
+    color: #FFF;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    div {
+        width: 70%;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;        
+        
+        padding: 10vmax 4vw;
+
+        margin: 0 auto;
+
+        & > * {
+            font-family: "Abel", sans-serif !important;
+
+            font-size: calc((1.8 - 1) * 1.2vw + 1rem);
+            
+            font-weight: 400;
+            line-height: 1.4em;
+            letter-spacing: 0;
+    
+            text-transform: none;
+            text-align: center;
+        }
+
+        p {
+            margin-bottom: 32px;
+        }
+    }
+`;
+
+export {
+    Container,
+    Navigator,
+    QuoteSection
+}
