006636a17813e9ba3aeaa90343c0c47fa1307682
Lucas <lucasazzollinivieira@gmail.com>
2022-05-17 23:51:01 -0300

    feat: AboutMePage layout done

--- src/pages/AboutMePage/style.tsx
+++ src/pages/AboutMePage/style.tsx
@@ -2,8 +2,56 @@ import styled from "styled-components";
 
 const Container = styled.main`
     width: 100%;
-    height: 100vh;
-    background-color: #3F61D7;
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    padding: 6.6vmax 4vw;
+
+    margin: 0 auto;
+
+    background-color: #FFF;
 `;
 
-export { Container }
+const Title = styled.h2`
+    font-family: "Abel", sans-serif;
+
+    font-size: calc((2.8 - 1) * 1.2vw + 1rem);
+    font-weight: 400;
+    letter-spacing: 0em;
+    text-transform: none;
+
+    text-align: center;
+    white-space: pre-wrap;
+
+    padding-bottom: 6.6vmax;
+`;
+
+const Text = styled.p`
+    width: 70vw;
+
+    font-family: "Source Code Pro", monospace;
+
+    font-size: calc((1.1 - 1) * 1.2vw + 1rem);
+    font-weight: 300;
+    line-height: 1.7;
+    letter-spacing: 0em;
+    
+    text-transform: none;
+
+    white-space: pre-wrap;
+    word-wrap: break-word;
+
+    margin: 1rem 0;
+    margin-top: 0;
+    
+    -webkit-font-smoothing: antialiased;
+
+`;
+
+export {
+    Container,
+    Title,
+    Text
+}
