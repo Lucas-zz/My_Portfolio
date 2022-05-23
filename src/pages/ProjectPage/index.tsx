77bbd510b18f15dc25e2fff7854087e012520b52
Lucas <lucasazzollinivieira@gmail.com>
2022-05-23 01:53:26 -0300

    feat: projects pages partially done

--- src/pages/ProjectPage/index.tsx
+++ src/pages/ProjectPage/index.tsx
@@ -1,9 +1,55 @@
-import { Container } from "./style";
+import { Contained, Container, Grid, More, Project, Title } from "./style";
+import globo from "../../assets/globo.svg"
+import TrackIt from "../../assets/TrackIt.svg";
+import MyWallet from "../../assets/MyWallet.svg";
+import MegaStore from "../../assets/MegaStore.png";
+import linkr from "../../assets/linkr.svg";
 
 export default function ProjectPage() {
     return (
         <>
-            <Container />
+            <Container>
+                <Grid>
+                    <Project>
+                        <Contained>
+                            <Title height={false}>
+                                <img src={globo} alt="globo.com logo" />
+                            </Title>
+                        </Contained>
+                    </Project>
+                    <Project>
+                        <Contained>
+                            <Title height={false}>
+                                <img src={TrackIt} alt="globo.com logo" />
+                            </Title>
+                        </Contained>
+                    </Project>
+                    <Project>
+                        <Contained>
+                            <Title height={true}>
+                                <img src={MyWallet} alt="globo.com logo" />
+                            </Title>
+                        </Contained>
+                    </Project>
+                    <Project>
+                        <Contained>
+                            <Title height={false}>
+                                <img src={MegaStore} alt="globo.com logo" />
+                            </Title>
+                        </Contained>
+                    </Project>
+                    <Project>
+                        <Contained>
+                            <Title height={false}>
+                                <img src={linkr} alt="globo.com logo" />
+                            </Title>
+                        </Contained>
+                    </Project>
+                    <More>
+                        More projects will be added soon!
+                    </More>
+                </Grid>
+            </Container>
         </>
     );
 }
--- src/pages/ProjectPage/style.tsx
+++ src/pages/ProjectPage/style.tsx
@@ -2,9 +2,121 @@ import styled from "styled-components";
 
 const Container = styled.main`
     width: 100%;
-    height: 100vh;
 
-    background-color: #3F61D7;
+    background-color: #FFF;
+
+    margin: 8vw auto;
 `;
 
-export { Container }
+const Grid = styled.section`
+    width: 100%;
+    min-height: 100%;
+
+    display: grid;
+    grid-template-columns: 45% 45%;
+    justify-content: space-evenly;
+
+    padding-top: 2%;
+
+    @media(max-width: 1024px) {
+        grid-template-columns: 80%;
+    }
+
+    @media(max-width: 767px) {
+        grid-template-columns: 100%;
+        padding-top: 10%;
+    }
+`;
+
+const Project = styled.article`
+    width: 100%;
+    height: 40vh;
+
+    background-color: #EFEFEF;
+
+    border-radius: 10px;
+    
+    margin: 5% 0;
+`;
+
+const Contained = styled.div`
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    -webkit-filter: grayscale(100%);
+    filter: grayscale(100%) blur(2px);
+    transition: .3s ease-in-out;
+    -webkit-transition:  .3s ease-in-out;
+
+    -webkit-transform: scale(1);
+	transform: scale(1);
+	-webkit-transition: .3s ease-in-out;
+	transition: .3s ease-in-out;
+
+    :hover {
+        -webkit-filter: grayscale(0);
+        filter: grayscale(0) blur(0);
+        -webkit-transform: scale(1.1);
+	    transform: scale(1.1);
+    }
+
+    @media(max-width: 767px) {
+        -webkit-filter: grayscale(0);
+        filter: grayscale(0) blur(0);
+        
+        :hover {
+            -webkit-filter: grayscale(0);
+            filter: grayscale(0) blur(0);
+            -webkit-transform: scale(1.1);
+	        transform: scale(1.1);
+    }
+    }
+`;
+
+const Title = styled.div<any>`
+    width: 70%;
+    height: 70%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & > img {
+        width: 60%;
+        height: 60%;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+`;
+
+const More = styled.div`
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-family: 'Abel', sans-serif;
+    font-size: calc((2 - 1) * 1.2vw + 1rem);
+
+    text-align: center;
+    word-wrap: break-word;
+
+    @media(max-width: 1024px) {
+        padding: 100px 0;
+    }
+`;
+
+export {
+    Container,
+    Grid,
+    Project,
+    Title,
+    Contained,
+    More
+}
