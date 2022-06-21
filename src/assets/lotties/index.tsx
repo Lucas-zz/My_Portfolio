6c62084483d067e9efd930524ed776df120ed33d
Lucas <lucasazzollinivieira@gmail.com>
2022-06-21 05:25:32 -0300

    feat: new Lotties and SVG added

---
+++ src/assets/lotties/index.tsx
@@ -0,0 +1,19 @@
+import * as html5 from './html-lottie.json';
+import * as css3 from './css3-lottie.json';
+import * as reactJS from './react-lottie.json';
+import * as javaScript from './javascript-lottie.json';
+import * as mongoDB from './mongo-lottie.json';
+import * as nodeJS from './nodejs-lottie.json';
+import * as webDev from './web-dev-lottie.json';
+
+const AnimationData = {
+    html5,
+    css3,
+    reactJS,
+    javaScript,
+    mongoDB,
+    nodeJS,
+    webDev,
+}
+
+export default AnimationData;
