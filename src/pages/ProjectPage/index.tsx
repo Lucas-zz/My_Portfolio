09cf8751f42b94dd9c6e097e4c47ffad67119bfa
Lucas <lucasazzollinivieira@gmail.com>
2022-06-23 16:15:01 -0300

    refactor: moved projects data and lotties to Utils folder and some minor styles modifications

--- src/pages/ProjectPage/index.tsx
+++ src/pages/ProjectPage/index.tsx
@@ -1,174 +1,62 @@
-import { Contained, Container, ContainerButtons, Grid, Icons, More, MUIStyles, Project, Title } from "./style";
-import globo from "../../assets/globo.svg"
-import TrackIt from "../../assets/TrackIt.svg";
-import MyWallet from "../../assets/MyWallet.svg";
-import MegaStore from "../../assets/MegaStore.png";
-import linkr from "../../assets/linkr.svg";
+import {
+    Contained,
+    Container,
+    ContainerButtons,
+    Grid,
+    Icons,
+    More,
+    MUIStyles,
+    Project,
+    Title,
+    Icon,
+    ProjectLogo
+} from "./style";
 import { useEffect } from "react";
-import { Button } from "@mui/material";
-import Lottie from 'react-lottie';
-import AnimationData from '../../assets/lotties/index';
+import { Button, Typography } from "@mui/material";
+
 import PostgreSQL from '../../assets/postgresql.svg';
 
-export default function ProjectPage() {
+import Lottie from 'react-lottie';
+import { projects } from "../../utils/projectUtils";
 
-    const projects = [
-        {
-            id: 1,
-            name: "Projeto - globo.com",
-            repoUrl: "https://github.com/Lucas-zz/globo.com",
-            appUrl: "",
-            image: globo,
-            technologies: [
-                { html: AnimationData.html5 },
-                { css: AnimationData.css3 },
-            ]
-        },
-        {
-            id: 2,
-            name: "Projeto - Track It",
-            repoUrl: "https://github.com/Lucas-zz/Track_It",
-            appUrl: "",
-            image: TrackIt,
-            technologies: [
-                { html: AnimationData.html5 },
-                { css: AnimationData.css3 },
-                { javaScript: AnimationData.javaScript },
-                { react: AnimationData.reactJS },
-                { node: AnimationData.nodeJS },
-            ]
-        },
-        {
-            id: 3,
-            name: "Projeto - My Wallet",
-            repoUrl: "https://github.com/Lucas-zz/MyWallet_frontEnd",
-            appUrl: "",
-            image: MyWallet,
-            technologies: [
-                { html: AnimationData.html5 },
-                { css: AnimationData.css3 },
-                { javaScript: AnimationData.javaScript },
-                { react: AnimationData.reactJS },
-                { node: AnimationData.nodeJS },
-                { mongo: AnimationData.mongoDB },
-            ]
-        },
-        {
-            id: 4,
-            name: "Projeto - Mega Store",
-            repoUrl: "https://github.com/Lucas-zz/mega-store-front",
-            appUrl: "",
-            image: MegaStore,
-            technologies: [
-                { html: AnimationData.html5 },
-                { css: AnimationData.css3 },
-                { javaScript: AnimationData.javaScript },
-                { react: AnimationData.reactJS },
-                { node: AnimationData.nodeJS },
-                { mongo: AnimationData.mongoDB },
-            ]
-        },
-        {
-            id: 5,
-            name: "Projeto - Linkr",
-            repoUrl: "https://github.com/Lucas-zz/Linkr-Front",
-            appUrl: "",
-            image: linkr,
-            technologies: [
-                { html: AnimationData.html5 },
-                { css: AnimationData.css3 },
-                { javaScript: AnimationData.javaScript },
-                { react: AnimationData.reactJS },
-                { node: AnimationData.nodeJS },
-                { postgresql: PostgreSQL, },
-            ]
-        },
-    ]
+export default function ProjectPage() {
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
 
-    const html = {
-        loop: true,
-        autoplay: true,
-        animationData: AnimationData.html5,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
-
-    const javaScript = {
-        loop: true,
-        autoplay: true,
-        animationData: AnimationData.javaScript,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
-
-    const react = {
-        loop: true,
-        autoplay: true,
-        animationData: AnimationData.reactJS,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
-
-    const node = {
-        loop: true,
-        autoplay: true,
-        animationData: AnimationData.nodeJS,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
-
-    const mongo = {
-        loop: true,
-        autoplay: true,
-        animationData: AnimationData.mongoDB,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
-
     return (
         <>
             <Container>
+                <Title>Projects</Title>
                 <Grid>
                     {projects.map((project) => (
                         <Project key={project.id}>
                             <Contained>
-                                <Title>
+                                <ProjectLogo>
                                     <img src={project.image} alt={project.name} />
-                                </Title>
+                                </ProjectLogo>
+                                <Typography variant="subtitle1" sx={MUIStyles.typography}>Developed with</Typography>
                                 <Icons>
-                                    {project.technologies.map(({ tech }: any) => (
-                                        <Lottie
-                                            options={{
-                                                loop: true,
-                                                autoplay: true,
-                                                animationData: tech,
-                                                rendererSettings: {
-                                                    preserveAspectRatio: "xMidYMid slice"
-                                                }
-                                            }}
-                                            width={66}
-                                            height={66}
-                                        />
-                                    ))
-
-                                    }
+                                    <Icon>
+                                        {project.technologies.map((tech: any) => (
+                                            tech === PostgreSQL ?
+                                                <img title="PostgreSQL" src={tech} alt="PostgreSQL database" />
+                                                :
+                                                <Lottie
+                                                    title={tech.title}
+                                                    options={tech}
+                                                />
+                                        ))}
+                                    </Icon>
                                 </Icons>
                                 <ContainerButtons>
-                                    <a href={project.repoUrl}>
+                                    <a href={project.repoUrl} target="_blank" rel="noreferrer">
                                         <Button sx={MUIStyles.button} variant="contained">
                                             Repository
                                         </Button>
                                     </a>
-                                    <a href={project.appUrl}>
+                                    <a href={project.appUrl} target="_blank" rel="noreferrer">
                                         <Button sx={MUIStyles.button} variant="contained">
                                             Application
                                         </Button>
