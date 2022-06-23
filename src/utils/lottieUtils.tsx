42af664a9e642797270bda44621776dbbfc9bf25
Lucas <lucasazzollinivieira@gmail.com>
2022-06-23 16:16:35 -0300

    feat: declaring and exporting lotties

---
+++ src/utils/lottieUtils.tsx
@@ -0,0 +1,61 @@
+import AnimationData from "../assets/lotties";
+
+export const HTML5 = {
+    loop: true,
+    autoplay: true,
+    animationData: AnimationData.html5,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    },
+    title: "HTML5",
+};
+
+export const CSS3 = {
+    loop: true,
+    autoplay: true,
+    animationData: AnimationData.css3,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    },
+    title: "CSS3",
+}
+
+export const JavaScript = {
+    loop: true,
+    autoplay: true,
+    animationData: AnimationData.javaScript,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    },
+    title: "JavaScript",
+};
+
+export const ReactJS = {
+    loop: true,
+    autoplay: true,
+    animationData: AnimationData.reactJS,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    },
+    title: "ReactJS",
+};
+
+export const NodeJS = {
+    loop: true,
+    autoplay: true,
+    animationData: AnimationData.nodeJS,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    },
+    title: "NodeJS",
+};
+
+export const MongoDB = {
+    loop: true,
+    autoplay: true,
+    animationData: AnimationData.mongoDB,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    },
+    title: "MongoDB",
+};
